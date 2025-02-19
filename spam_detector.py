7c4877878dabb66f3b86a26981f18ed026bcdcc8
griffgaff <bolajibaqi@gmail.com>
2025-02-19 08:25:05 +0100

    loading spam model

--- spam_detector.py
+++ spam_detector.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import pickle
+import config
 
 def train_detector():
     data = pd.read_csv("spam_dataset.csv")  # Dataset should contain 'text' and 'label' columns
@@ -14,3 +15,12 @@ def train_detector():
 
     with open("spam_model.pkl", "wb") as f:
         pickle.dump((vectorizer, model), f)
+
+def load_spam_model():
+    with open(config.SPAM_MODEL, "rb") as f:
+        return pickle.load(f)
+
+def is_spam_email(email_body):
+    vectorizer, model = load_spam_model()
+    email_vector = vectorizer.transform([email_body])
+    return model.predict(email_vector)[0] == "spam"
